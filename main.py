4556c2f6324cd5179b70d9c9db811c13ea7d6fc0
huseyin <h.ozturk1216@gmail.com>
2022-10-13 00:58:44 +0300

    foto isimleri aynı kalarak boyutu indirgendi

---
+++ main.py
@@ -0,0 +1,25 @@
+
+from os import listdir
+from os.path import isfile, join
+import cv2
+import numpy as np
+import numpy as np
+import glob
+import os
+
+
+mypath='./segmentasyon_haritası/'
+os.mkdir("ResizedSegmentasyon_haritasi")
+folderLen=len(mypath)
+
+for img in glob.glob(mypath+"/*.jpg"):
+  image=cv2.imread(img)
+  imgResized=cv2.resize(image,(1024,1024))
+  cv2.imwrite("ResizedSegmentasyon_haritasi/"+img[folderLen:],imgResized)
+  cv2.imshow("image", imgResized)
+  cv2.waitKey(1)
+cv2.destroyAllWindows()
+
+
+
+
